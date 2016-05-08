4108237c3c672c9700f9707f891d233a017ecb00
Patricio <patricio.vargas@usach.cl>
2016-05-08 10:10:40 -0300

    ultimos ajustes

--- funciones.py
+++ funciones.py
@@ -42,20 +42,20 @@ Entrada: valor minimo y maximo para el corte, la frecuencia de muestreo y el ord
 Salida: a y b, el minimo y el maximo para el corte
 Funcion: encontrar los límites del corte
 """
-def butter_bandpass(lowcut, highcut, rate, order=5):
-    nyq = 0.5 * rate
-    low = lowcut / nyq
-    high = highcut / nyq
-    b, a = butter(order, [low, high], btype='band')
+def bandpass(lowcut, highcut, rate, order=5):
+    rate_medio = 0.5 * rate
+    inferior = lowcut / rate_medio
+    superior = highcut / rate_medio
+    b, a = butter(order, [inferior, superior], btype='band')
     return b, a
 
 """
 Entrada: valor minimo y maximo para el corte, la frecuencia de muestreo, el orden para el filtro y la señal
 Salida: señal filtrada
-Funcion: realizar el filtro a la señal original. Requiere de los servicios de butter_bandpass
+Funcion: realizar el filtro a la señal original. Requiere de los servicios de bandpass
 """
-def butter_bandpass_filter(data, lowcut, highcut, rate, order=5):
-    b, a = butter_bandpass(lowcut, highcut, rate, order=order)
+def filtro(data, lowcut, highcut, rate, order=5):
+    b, a = bandpass(lowcut, highcut, rate, order=order)
     y = lfilter(b, a, data)
     return y
 
@@ -82,7 +82,7 @@ def aplicarFiltro(lowcut,highcut,signal,rate):
 	k = arange(largo_n) #crear un arreglo del tamaño de la señal
 	T = largo_n/rate #Periodo es igual al largo partido la frecuencia
 	frq = k/T #Se asigna una frecuencia a cada elemento de k
-	y = butter_bandpass_filter(signal, lowcut, highcut,rate, order=1)
+	y = filtro(signal, lowcut, highcut,rate, order=1)
 	plt.plot(frq, y)
 	plt.grid(True)
 	plt.axis('tight')
